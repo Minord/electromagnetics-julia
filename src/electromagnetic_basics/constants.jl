775bd56b56ec947053b258733f7bcefbad56bc3e
Minord <benderrobot12@gmail.com>
2021-12-01 20:16:09 -0600

    Move and add more electric relevant constants

---
+++ src/electromagnetic_basics/constants.jl
@@ -0,0 +1,51 @@
+#Electric Permitivity of Vaccum
+const ε0 = 8.8541878128e-12
+#Magnetic Permeability of Vaccum
+const μ0 = 4e-7*π
+#Speed of Ligth
+const c = 299792458
+
+#Precaculated commond factor
+const k_coulomb = 1 / (4π*ε0)
+const m_magnetic = 1 / (2π*μ0)
+
+#Volt Units
+const MV = 1e6
+const kV = 1e3
+const V = 1.0
+const mV = 1e-3
+const μV = 1e-6
+const nV = 1e-9
+
+#Coulomb Units
+const C = 1.0
+const mC = 1e-3
+const μC = 1e-6
+const nC = 1e-9
+const pC = 1e-12
+
+#Farad Units
+const F = 1.0
+const mF = 1e-3
+const μF = 1e-6
+const nF = 1e-9
+const pF = 1e-12
+
+#Herry Units
+const MH = 1e6
+const kH = 1e3
+const H = 1.0
+const mH = 1e-3
+const μH = 1e-6
+const nH = 1e-9
+const pH = 1e-12
+
+#Meter Units
+const km = 1e3
+const m = 1.0
+const cm = 1e-2
+const mm = 1e-3
+const μm = 1e-6
+const nm = 1e-9
+const ̊A = 1e-10
+const pm = 1e-12
